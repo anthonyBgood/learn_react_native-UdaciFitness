9c2b0ee7f1dc0162cdeda1d3b1765eefb06c7d0b
anthonyBgood <anthonyBpostbox@gmail.com>
2019-06-11 23:51:29 +1000

    icons rendering

--- App.js
+++ App.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AddEntry from './components/AddEntry'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>working on the app Now. Open up App.js to start working on your app!</Text>
+    <View>
+      
+      <AddEntry />
+      
+
+      
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
