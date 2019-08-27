d7ad74fdf508f6a12cb0b12b9e4b59c0b0802102
anthonyBgood <anthonyBpostbox@gmail.com>
2019-08-28 00:24:07 +1000

    removed package vulnerabilities

--- App.js
+++ App.js
@@ -25,64 +25,69 @@ function UdaciStatusBar ({backgroundColor, ...props}) {
   )
 }
 
-const Tabs = createBottomTabNavigator({
-
-  History:{
-    screen: () => <History />,
-    navigationOptions:{
-      tabBarLabel: 'History',
-      tabBarIcon: ({ tintColor}) => 
-            <Ionicons 
-              name='ios-bookmarks' 
-              size={30}
-              color={tintColor}/>
-    } , 
-  } ,
-  AddEntry:{
-    screen: () => <AddEntry/> ,
-    navigationOptions: {
-      tabBarLabel: 'Add Entry' ,
-      tabBarIcon: ({ tintColor}) =>
-        <FontAwesome 
-          name='plus-square'
-          size={30} 
-          color={tintColor} /> 
-    },
-  },
-},{
-  navigationOptions:{
-    header:null
-  },
-  tabBarOptions: {
-    activeTintColor: Platform.OS === 'ios' ? purple :white,
-    style: {
-      height: 56,
-      backgroundColor: Platform.OS === 'ios' ?white :purple,
-      shadowColor: 'rgba(0,0,0,0.24)',
-      shadowOffset: {
-        width:0,
-        height: 3
+const Tabs = createBottomTabNavigator(
+  {
+      History:{
+        screen: () => <History />,
+        navigationOptions:{
+          tabBarLabel: 'History',
+          tabBarIcon: ({ tintColor}) => 
+                <Ionicons 
+                  name='ios-bookmarks' 
+                  size={30}
+                  color={tintColor}/>
+        } , 
+      } ,
+      AddEntry:{
+        screen: () => <AddEntry/> ,
+        navigationOptions: {
+          tabBarLabel: 'Add Entry' ,
+          tabBarIcon: ({ tintColor}) =>
+            <FontAwesome 
+              name='plus-square'
+              size={30} 
+              color={tintColor} /> 
+        },
       },
-      shadowRadius: 6,
-      shadowOpacity: 1
-    }
-  }
-})
-
-const MainNavigator = createStackNavigator({
-  Home:{
-    screen:Tabs,
   },
-  EntryDetail:{
-    screen: EntryDetail, 
+
+  {
     navigationOptions:{
-      headerTintColor: white, 
-      headerStyle: {
-        backgroundColor: purple,
+      header:null
+    },
+    tabBarOptions: {
+      activeTintColor: Platform.OS === 'ios' ? purple :white,
+      style: {
+        height: 56,
+        backgroundColor: Platform.OS === 'ios' ?white :purple,
+        shadowColor: 'rgba(0,0,0,0.24)',
+        shadowOffset: {
+          width:0,
+          height: 3
+        },
+        shadowRadius: 6,
+        shadowOpacity: 1
       }
     }
   }
-})
+)
+
+const MainNavigator = createStackNavigator(
+  {
+    Home:{
+      screen:Tabs,
+    },
+    EntryDetail:{
+      screen: EntryDetail, 
+      navigationOptions:{
+        headerTintColor: white, 
+        headerStyle: {
+          backgroundColor: purple,
+        }
+      }
+    }
+  }
+)
 
 const MainAppContainer = createAppContainer(MainNavigator)  
 
